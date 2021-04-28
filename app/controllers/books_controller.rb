627ee2b70d8fbaf76e35ca00bf95dca81b8134c5
Ryonono <ryononosongs3746@docomo.ne.jp>
2021-04-28 07:00:34 +0000

    migration

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,13 @@
 class BooksController < ApplicationController
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    @book = Book.new
+    @books = Book.all
+    @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
     else
@@ -16,15 +17,26 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+
+  private
+
+  def book_params
+   params.require(:book).permit(:title, :body)
+  end
+
+
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,16 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update

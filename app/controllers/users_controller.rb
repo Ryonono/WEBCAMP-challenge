382017aa98e2e775ec5d4cdc53f96a6ab6e2f423
Ryonono <ryononosongs3746@docomo.ne.jp>
2021-04-29 07:35:48 +0000

    we can update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end

c016bb095062db203f186cb51deca24c7f3bc958
Ryonono <ryononosongs3746@docomo.ne.jp>
2021-04-29 10:14:22 +0000

    added image

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7af16b478840cec063657130d751bcb6631a79c2fb98e788fea185809604946cb0899cae87db59aef08849be193ca7efa49000adc54f4d2843b2f901f07ec77c'
